7186291a78c808ea190f4bfe794acd2b0cd90581
sanjeev singh <snjvsingh123@gmail.com>
2017-11-12 17:00:48 -0500

    backup

--- q2_protozoan.rb
+++ q2_protozoan.rb
@@ -5,11 +5,44 @@ require_relative 'stats'
 require_relative 'sample_distrib'
 require_relative 'random_gaussian'
 
+Banner = <<EOF
+A distribtution of protozoan is normal with a mean of 46.7 days and std dev of 3.2 days.
+a) Find the prob that a protozoan selected at random will live for at least 52 days.
+b) Find the prob that a SRS of 25 protozoan will have a mean expectancy of at least 53 days.
+Usage: q2_protozoan.rb [options]
+EOF
+
 def main(options)
   population = RandomGaussian.new(options[:mean], options[:std_dev]).population(options[:population_size])
 
-  sample_stats = run_sample_trials(population, options)
+  sample_stats = run_sample_trials(population, options) do |lifetime|
+    (lifetime <= options[:mean]) ? 1 : 0
+  end
+  puts %Q[Chk)Probability that sample of #{options[:sample_size]} will be true for lifetime <= #{options[:mean]}\
+        days is  #{sample_stats.mean}"]
 
+  options[:lifetime] = 52
+  options[:result_file] = "q2_a.txt"
+  sample_stats = run_sample_trials(population, options) do |lifetime|
+    (lifetime >= options[:lifetime]) ? 1 : 0
+  end
+
+  puts %Q[a)Probability that sample of #{options[:sample_size]} will be true for lifetime >= #{options[:lifetime]}\
+        days is  #{sample_stats.mean}"]
+
+  
+  options[:lifetime] = 53
+  options[:result_file] = "q2_b.txt"
+  options[:num_samples] = 25
+  options[:num_trials] = 1
+  result = []
+  sample_stats = run_sample_trials(population, options) do |lifetime|
+    result << lifetime
+    (lifetime >= options[:lifetime]) ? 1 : 0
+  end
+  puts result
+  puts "Mean number in sample is #{result.mean}"
+  puts "Std dev of number in sample is #{result.std_dev}"
 end
 
 # Run trials to compute the sample values
@@ -19,10 +52,11 @@ def run_sample_trials(population, options)
 
   options[:num_trials].times do |i|
     num_successes = run_trial(options[:num_samples], population) do |lifetime|
-      lifetime >= options[:lifetime] ? 1 : 0
+      yield(lifetime)
     end
     freq_successes[num_successes] += 1 
-    result << num_successes 
+    result << num_successes
+    (options[:num_samples]-1).times { result << 0 }
   end
 
   if(options[:result_file])
@@ -51,34 +85,30 @@ def run_trial(num_samples, population)
 end
 
 def create_options
-  options = {num_samples: 5, num_trials: 1000000, result_file: nil, lifetime: 52,
+  options = {num_samples: 1, num_trials: 1000000, result_file: nil, lifetime: 52,
              population_size: 1000000, mean: 46.7, std_dev: 3.2}
   
   parser = OptionParser.new do |opts|
-    opts.banner = <<EOF
-A distribtution of protozoan is normal with a mean of 46.7 days and std dev of 3.2 days.
-Find the prob that a protozoan selected at random will live for at least 52 days.
-Usage: q2_protozoan.rb [options]
-EOF
-    opts.on('-n', '--num_samples') do |num_samples|
-      options[:num_samples] = num_samples
+    opts.banner = Banner
+    opts.on('-n NumSamples', '--num_samples n') do |num_samples|
+      options[:num_samples] = num_samples.to_i
     end
-    opts.on('-t', '--num_trials') do |num_trials|
+    opts.on('-t Trails', '--num_trials n') do |num_trials|
       options[:num_trials] = num_trials.to_i
     end
-    opts.on('-o', '--output') do |reuslt_file|
+    opts.on('-o file', '--output filename') do |result_file|
       options[:result_file] = result_file 
     end
-    opts.on('-l', '--lifetime') do |lifetime|
+    opts.on('-l Lifetime', '--lifetime N') do |lifetime|
       options[:lifetime] = lifetime.to_f
     end
-    opts.on('-p', '--population_size') do |population_size|
-      options[:lifetime] = population_size.to_i
+    opts.on('-p PopulationSize', '--population_size N') do |population_size|
+      options[:population_size] = population_size.to_i
     end
-    opts.on('-m', '--mean') do |mean|
+    opts.on('-m Mean', '--mean N') do |mean|
       options[:mean] = mean.to_f
     end
-    opts.on('-s', '--std_dev') do |std_dev|
+    opts.on('-s StdDev', '--std_dev F') do |std_dev|
       options[:std_dev] = std_dev.to_f
     end
   end.parse!
